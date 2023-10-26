33bd1eefec8ef4af1b1c932298c12c5244c836d0
MariaGlynou <mariasglynou@gmail.com>
2023-10-26 10:39:20 +0300

    Stripe charge

--- functions.php
+++ functions.php
@@ -11,3 +11,15 @@ function enqueue_child_styles() {
 
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
 }
+
+add_filter( 'gform_stripe_charge_authorization_only', 'stripe_charge_authorization_only', 10, 2 );
+function stripe_charge_authorization_only( $authorization_only, $feed ) {
+    gf_stripe()->log_debug( __METHOD__ . '(): Running... ');
+    $feed_name  = rgars( $feed, 'meta/feedName' );
+    if ( $feed_name == 'Stripe Feed - ddc' ) {
+        gf_stripe()->log_debug( __METHOD__ . '(): Authorization only charge for feed ' . $feed_name );
+        return true;
+    }
+  
+    return $authorization_only;
+}
