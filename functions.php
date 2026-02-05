c4a7f1d08d630b032feb8c62ae12d22b28a6bb58
Nikos Roussos <nikos.roussos@greenpeace.org>
2026-02-05 11:10:05 +0200

    Use Greek analyzer for ElasticSearch

--- functions.php
+++ functions.php
@@ -4,9 +4,15 @@
  * Additional code for the child theme goes in here.
  */
 
-add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
+add_action('wp_enqueue_scripts', 'enqueue_child_styles', 99);
 function enqueue_child_styles() {
 	$css_creation = filectime(get_stylesheet_directory() . '/style.css');
 
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
+	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation);
 }
+
+// Use Greek analyzer for ElasticSearch.
+// Ref: https://github.com/10up/ElasticPress/issues/1132
+add_filter('ep_analyzer_language', static function (): string {
+	return 'greek';
+});
